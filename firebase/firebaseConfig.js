efd41c2c0a222aa1d172dea40aa6d55f2c8c51f8
Syeda Kiswa <syedakiswa710@gmail.com>
2025-12-30 22:30:08 +0500

    Inital commit

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBZGGpV4CbZqMfVwRuwT0xM93t_zbwZbo",
-    authDomain: "job-board-2a090.firebaseapp.com",
-    projectId: "job-board-2a090",
-    storageBucket: "job-board-2a090.firebasestorage.app",
-    messagingSenderId: "452191248730",
-    appId: "1:452191248730:web:638a62b375feb5245ad363",
-    measurementId: "G-8F0E4PT9F2"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
