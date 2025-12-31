d681308879bdbd115c0db54ed2c9975ab9774c72
Syeda Kiswa <syedakiswa710@gmail.com>
2025-12-31 15:01:44 +0500

    Inital commit

--- app/apply/page.jsx
+++ app/apply/page.jsx
@@ -1,8 +1,5 @@
-
-import dynamic from 'next/dynamic'
-
-
-const ApplyClient = dynamic(() => import('./ApplyClient'), { ssr: false })
+'use client'  
+import ApplyClient from './ApplyClient'
 
 export default function Page() {
   return <ApplyClient />
